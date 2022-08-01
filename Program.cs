1ee1f2ac2a0d16c570913be448a66b8651775c21
Rodrigo Lopes <52975315+rodrigoczlopes@users.noreply.github.com>
2022-08-01 17:17:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais2");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+            bool grupo = true;
+            bool acompanhado = quantidadePessoas > 1;
+
+            if (idadeJoao >= 18 || acompanhado )
+            {
+                Console.WriteLine("Pode entrar!");
+            }
+            else
+            {
+               
+                Console.WriteLine("Não pode entrar!");
+                
+
+            }
+            Console.WriteLine("Tecle enter para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
